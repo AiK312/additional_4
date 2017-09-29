ae4326bb1b722b14f85583bdbb050a6903fb261c
Евгений <eugene.kurlenya@gmail.com>
2017-09-29 17:45:19 +0300

    Приведём к нормальному виду, а то некрасиво!

--- src/index.js
+++ src/index.js
@@ -1,55 +1,37 @@
-module.exports = function multiply(first, second) 
-{
-    let arrayFirst = first.split('').reverse();
-    let arraySecond = second.split('').reverse();    
+module.exports = function multiply(first, second) {
+  let arrayFirst = first.split('').reverse();
+  let arraySecond = second.split('').reverse();
+  let result = [];
+  let decade = 0;
+  let decadeIfResMoreTen = 0;
 
-    let result = [];
-    let decade = 0;
-    let decadeIfResMoreTen = 0;
-    
-    for(let i = 0; i<arrayFirst.length+arraySecond.length; i++)
-    {
-        result[i] = 0;
+  for (let i = 0; i < arrayFirst.length + arraySecond.length; i++) {
+    result[i] = 0;
+  }
+
+  for (let i = 0; i < arraySecond.length; ++i) {
+    decade = 0;
+    decadeIfResMoreTen = 0;
+
+    for (let j = 0; j < arrayFirst.length; ++j) {
+
+      result[i + j] += ((arrayFirst[j] * arraySecond[i] + decade) % 10);
+      decade = Math.floor((arrayFirst[j] * arraySecond[i] + decade) / 10);
+
+      if (result[i + j] >= 10) {
+        result[i + j] = result[i + j] % 10;
+        decade += 1;
+      }
+      if ((j + 1) == arrayFirst.length) {
+        result[i + j + 1] = decade;
+      }
     }
 
-    for(let i = 0; i < arraySecond.length; ++i)
-    { 
-        decade = 0;
-        decadeIfResMoreTen = 0;
+  }
 
-        for(let j = 0; j < arrayFirst.length; ++j)
-        {  
-        	
-        	  result[i+j] += ((arrayFirst[j] * arraySecond[i] + decade) % 10);
-            	decade = Math.floor((arrayFirst[j] * arraySecond[i] + decade) / 10);
-            	
-            	if(result[i+j] >= 10)
-            	{
-            		result[i+j] = result[i+j] % 10;
-            		decade += 1;
-            	} 
-            	
-            //	alert(result[i+j] + "..... " +decade);
-            
-            if((j+1) == arrayFirst.length)
-            {                
-                result[i+j+1] = decade;
-            }
-
-           
-            
-            //alert(i+j + "..... " + result + "..... "+ decade +
-           // "..... " + decadeIfResMoreTen);     
-           // alert('\t' + decade);      
-        }    
-        
-    }
-
-    result.reverse();
-    if(result[0] == 0)
-    {
-        result.shift();
-    }
-    return result.join('');
-               
+  result.reverse();
+  if (result[0] == 0) {
+    result.shift();
+  }
+  return result.join('');
 } 
